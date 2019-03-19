1dfe3dbf62de8296af27a3a1518e8df4b3c27a0d
Alex Tea <alextea2@gmail.com>
2019-03-19 17:02:06 +0100

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map></Map>
   </div>
 </template>
 
 <script>
+
 import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/MapComponent'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Map
   }
 }
 </script>
